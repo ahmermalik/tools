81aa2964585b5c65a2bf71bf9505821f9e859970
Ahmer Malik <ahmer9800@gmail.com>
2018-06-19 15:59:25 -0500

    learned to multiply canvas

--- 3)chart/admin.js
+++ 3)chart/admin.js
@@ -6,8 +6,26 @@ var jsonfile = {
         "age": 12
     }, {
         "name": "Tom",
+        "age": 25
+    },{
+        "name": "Tom",
+        "age": 32
+    },{
+        "name": "Tom",
         "age": 14
-    }]
+    },{
+        "name": "Tom",
+        "age": 10
+    },{
+        "name": "Tom",
+        "age": 2
+    },{
+        "name": "Tom",
+        "age": 5
+    },{
+        "name": "Tom",
+        "age": 26
+    },]
 };
 
 var labels = jsonfile.jsonarray.map(function(e) {
@@ -17,7 +35,60 @@ var data = jsonfile.jsonarray.map(function(e) {
     return e.age;
 });;
 
-var ctx = canvas.getContext('2d');
+var ctx = document.getElementById('canvas').getContext('2d');
+var config = {
+    type: 'line',
+    data: {
+        labels: labels,
+        datasets: [{
+            label: 'Graph Line',
+            data: data,
+            backgroundColor: 'rgba(0, 119, 204, 0.3)'
+        }]
+    }
+};
+
+var chart = new Chart(ctx, config);
+
+
+
+
+var jsonfile = {
+    "jsonarray": [{
+        "name": "Joe",
+        "age": 12
+    }, {
+        "name": "Tom",
+        "age": 25
+    },{
+        "name": "Tom",
+        "age": 32
+    },{
+        "name": "Tom",
+        "age": 14
+    },{
+        "name": "Tom",
+        "age": 10
+    },{
+        "name": "Tom",
+        "age": 2
+    },{
+        "name": "Tom",
+        "age": 5
+    },{
+        "name": "Tom",
+        "age": 26
+    },]
+};
+
+var labels = jsonfile.jsonarray.map(function(e) {
+    return e.name;
+});
+var data = jsonfile.jsonarray.map(function(e) {
+    return e.age;
+});;
+
+var ctx = document.getElementById('canvastwo').getContext('2d');
 var config = {
     type: 'line',
     data: {
