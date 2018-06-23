29d0d0f72ef3636ea6e5af3a9195cd24f1dd8d5f
Ahmer Malik <ahmer9800@gmail.com>
2018-06-23 17:35:39 -0500

    clean code and update to ES6 and tracer

--- 2)tenMinuteVolatility/tenMinVolatility.js
+++ 2)tenMinuteVolatility/tenMinVolatility.js
@@ -1,23 +1,22 @@
 /**Script Written by Ahmer Malik 6.14.2018 **/
 
 const axios = require('axios');
+const console = require('tracer').console();
 var prompt = require('prompt-promise'); // for accepting user input - promise based
 
 function Main() {
-    let getUserInputs = new Promise((resolve, reject) => {
-            prompt('What coin do you want to analyze: ')
-                .then((coin) => {
-                    prompt.finish();
+    prompt('What coin do you want to analyze: ')
+        .then((coin) => {
+            prompt.finish();
 
-                    return getMarketData(coin);     //you send the function to get json data.
-                })
-                .then((marketData) => {             //you are resolving the promise 'getMarketData', by giving it the 'marketData' which was returned. the naming convention doesn't matter when you're passing data/arguments through a function.
-                    console.log(marketData);
-                })
-                .catch(function (error) {
-                    reject(error);
-                    prompt.finish();
-                });
+            return getMarketData(coin);     //you send the function to get json data.
+        })
+        .then((marketData) => {             //you are resolving the promise 'getMarketData', by giving it the 'marketData' which was returned. the naming convention doesn't matter when you're passing data/arguments through a function.
+            console.log(marketData);
+        })
+        .catch(function (error) {
+            console.log(error);
+            prompt.finish();
         });
 };
 
@@ -27,7 +26,7 @@ function Main() {
  * @param limit {int} Time frame
  * @return {Promise} // TODO: describe this
  */
-function getMarketData(coin, limit = -1) {
+function getMarketData(coin, limit = 10) {
     return new Promise((resolve, reject) => {
         const url = `https://min-api.cryptocompare.com/data/histominute?fsym=${coin.toUpperCase()}&tsym=USD&limit=${limit}`;
         const config = {
@@ -37,7 +36,6 @@ function getMarketData(coin, limit = -1) {
         axios.get(url, config)
             .then(function (response) {
                 resolve(response.data['Data']);     // resolve = function passed in as the next ".then(resolve{...})"
-
             })
             .catch(reject);
     });
